1ef12ff9f2b87de3b838471cc47814e163780860
vapvin <vins@vins.dev>
2020-08-26 21:29:20 +0900

    Complete

--- src/number.rs
+++ src/number.rs
@@ -11,21 +11,26 @@ fn main() {
 
     io::stdin().read_line(&mut user)
     .expect("입력에 실패하였습니다.");
-
-    let input_value: u32 = user_input(user);
     let secret_value: u32 = create_random_number();
 
-    println!("{} {}", input_value, secret_value);
-
-    match input_value.cmp(&secret_value){
-        Ordering::Less => println!("입력한 숫자가 정답보다 낮습니다."),
-        Ordering::Greater => println!("입력한 숫자가 정답보다 큽니다."),
-        Ordering::Equal => println!("입력한 숫자와 정답이 일치합니다."),
-
+    loop {
+        let input_value: u32 = user_input(&user);
+    
+        println!("{} {}", input_value, secret_value);
+    
+        match input_value.cmp(&secret_value){
+            Ordering::Less => println!("입력한 숫자가 정답보다 낮습니다."),
+            Ordering::Greater => println!("입력한 숫자가 정답보다 큽니다."),
+            Ordering::Equal => {
+                println!("입력한 숫자와 정답이 일치합니다.");
+                break;
+            },
+    
+        }
     }
 }
 
-fn user_input(user: String) -> u32{
+fn user_input(user: &String) -> u32{
     println!("---------- {}님 반갑습니다. ----------", user);
     println!("---------- 랜덤한 숫자가 설정되었습니다. ----------");
     println!("---------- 정답을 입력하여 주세요. ----------");
