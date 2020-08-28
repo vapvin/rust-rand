6dc23e2e2f227ad6c82da6d4a859dc219a8555ce
vapvin <vins@vins.dev>
2020-08-28 19:24:34 +0900

    Bool Type

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 fn main(){
-    let sum = 5 + 10;
+    let t = true;
 
-    let defference = 95.5 - 4.3;
-
-    let product = 4 * 30;
-
-    let quotient = 56.7 / 32.2;
-
-    let remainder = 43 % 5;
+    let f: bool = false;
 }
