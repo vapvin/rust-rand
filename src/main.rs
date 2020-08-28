bbc8e800c16019e6fae69e76595a28d00b9ba145
vapvin <vins@vins.dev>
2020-08-28 19:22:57 +0900

    사칙연산

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main(){
-    let x = 2.0;
+    let sum = 5 + 10;
 
-    let y: f32 = 3.0;
+    let defference = 95.5 - 4.3;
+
+    let product = 4 * 30;
+
+    let quotient = 56.7 / 32.2;
+
+    let remainder = 43 % 5;
 }
