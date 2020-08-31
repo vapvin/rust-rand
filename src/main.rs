e4d726b749b738b98590ed311e07a24993c76873
vapvin <vins@vins.dev>
2020-08-31 21:42:47 +0900

    Area fn

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool
+fn main() {
+    let width = 30;
+    let height = 50;
+    print!(
+        "사각형의 면적: {} 제곱픽셀",
+        area(width1, height1)
+    );
 }
-fn main(){
-    let user1: User = User{
-        email: String::from("vins@vins.dev"),
-        username: String::from("vins"),
-        active: true,
-        sign_in_count: 1,
-    };
-};
+
+fn area(width: u32, height: u32)-> u32 {
+    width * height
+}
