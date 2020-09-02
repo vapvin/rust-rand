82cfd25b27c5952f4e843cbb55d30e6f43b6f5bb
vapvin <vins@vins.dev>
2020-09-02 20:25:50 +0900

    ownerships 02

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 fn main(){
-    let s = String::from("hello");
+    let s1 = gives_ownership();
 
-    takes_ownership(s);
+    let s2 = String::from("hello");
 
-    let x = 5;
-
-    makes_copy(x);
+    let s3 = takes_and_gives_back(s2);
 }
 
-fn takes_ownership(some_string: String){
-    println!("{}", some_string);
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+
+    some_string
 }
 
-fn makes_copy(some_integer: i32){
-    println!("{}", some_integer);
+fn take_and_gives_back(a_string: String) -> String {
+    a_string
 }
