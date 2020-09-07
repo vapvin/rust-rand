92ef94871b8bfde0272254db64f4fe40c2af0963
vapvin <tpehdj12@daum.net>
2020-09-07 22:24:11 +0900

    mod

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-fn plus_one(x: Option<i32>) -> Option(i32) {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
     }
 }
-
-let five = Some(5);
-let six = plus_one(five);
-let none = plus_one(None);
