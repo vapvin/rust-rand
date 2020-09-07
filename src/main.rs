52a7488a73eecd821eeb004e448eba337f98bdd7
vapvin <tpehdj12@daum.net>
2020-09-07 22:25:38 +0900

    Related and Absolute

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,9 @@ mod front_of_house {
         fn add_to_waitlist() {}
     }
 }
+
+pub fn eat_at(){
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
+}
