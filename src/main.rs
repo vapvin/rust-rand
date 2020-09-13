0589f5aecea66238e2784f43107df5f3f1891113
vapvin <vins@vins.dev>
2020-09-13 18:30:56 +0900

    Default Syntax

--- src/main.rs
+++ src/main.rs
@@ -75,3 +75,11 @@ fn main(){
 
 }
 
+fn build_user(email: String, username: String) -> User {
+    User{
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
