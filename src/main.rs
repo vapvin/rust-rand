950703806f78cb3c79ff2b5e2dc05cedee3ad5c1
vapvin <vins@vins.dev>
2020-09-15 21:36:43 +0900

    Local IP

--- src/main.rs
+++ src/main.rs
@@ -109,3 +109,13 @@ fn route(ip_type: IpAddrKind) {}
 
 route(IpAddrKind::V4);
 route(IpAddrKind::V6);
+
+struct  IpAddr {
+    kind: IpAddrKind,
+    address: String
+}
+
+let home = IpAddr {
+    kind: IpAddrKind::V4,
+    address: String::from("127.0.0.1"),
+}
