40d9f1351d449179cb201bc5393e6170591d1d7d
vapvin <vins@vins.dev>
2020-09-15 21:40:16 +0900

    Loop Back

--- src/main.rs
+++ src/main.rs
@@ -119,3 +119,8 @@ let home = IpAddr {
     kind: IpAddrKind::V4,
     address: String::from("127.0.0.1"),
 }
+
+let loopback = IpAddr {
+    kind: IpAddrKind::V6,
+    address: String::from("::1"),
+}
